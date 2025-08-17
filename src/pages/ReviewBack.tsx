c152b16325eca061313a11da3112c1d5c0413f95
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-08-17 22:37:10 +0900

    ページ分割

---
+++ src/pages/ReviewBack.tsx
@@ -0,0 +1,10 @@
+// ブラックジャック解説 後半ページ
+import ReviewTOC from "./ReviewTOC";
+// ...セクション・レイアウト・TOC用items配列を後で追加...
+export default function ReviewBack() {
+  return (
+    <div>
+      {/* TOCと本文をここに配置 */}
+    </div>
+  );
+}
---
+++ src/pages/ReviewBonus.tsx
@@ -0,0 +1,10 @@
+// ブラックジャック解説 おまけページ
+import ReviewTOC from "./ReviewTOC";
+// ...セクション・レイアウト・TOC用items配列を後で追加...
+export default function ReviewBonus() {
+  return (
+    <div>
+      {/* TOCと本文をここに配置 */}
+    </div>
+  );
+}
