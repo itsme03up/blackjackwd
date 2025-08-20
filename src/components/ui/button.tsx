6b308fd7bd91ababfbf69bf6157158a109a02502
Anna Yanchuk <itsme03anna.yanchuk@ognivo.net>
2025-08-20 22:29:57 +0900

    refactor: remove duplicate import of Slot in Button component for cleaner code

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,6 +1,5 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
-import { Slot } from "@radix-ui/react-slot"
 import { cn } from "../../lib/utils"
 import { buttonVariants } from "./button-variants"
 import type { ButtonVariantProps } from "./button-variants"
